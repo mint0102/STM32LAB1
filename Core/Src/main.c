d1bcef340806ffc49d9f06de0f9f11f9649eacd3
mint0102 <hahuyen010102@gmail.com>
2023-09-12 16:57:46 +0700

    ex3

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -66,57 +66,45 @@ int count_yellow = 2;
 int count_green = 3;
 int counter = 0;
 
-void Phase1_GreenOn()
-{
+void Phase1_GreenOn(){
 	HAL_GPIO_WritePin(GREEN_1_GPIO_Port, GREEN_1_Pin, GPIO_PIN_RESET);
 }
-void Phase1_GreenOff()
-{
-	HAL_GPIO_WritePin(GREEN_1_GPIO_Port, GREEN_1_Pin, GPIO_PIN_SET);;
+void Phase1_GreenOff(){
+	HAL_GPIO_WritePin(GREEN_1_GPIO_Port, GREEN_1_Pin, GPIO_PIN_SET);
 }
 
-void Phase1_YellowOn()
-{
+void Phase1_YellowOn(){
 	HAL_GPIO_WritePin(YELLOW_1_GPIO_Port, YELLOW_1_Pin, GPIO_PIN_RESET);
 }
-void Phase1_YellowOff()
-{
+void Phase1_YellowOff(){
 	HAL_GPIO_WritePin(YELLOW_1_GPIO_Port, YELLOW_1_Pin, GPIO_PIN_SET);
 }
 
-void Phase1_RedOn()
-{
+void Phase1_RedOn(){
 	HAL_GPIO_WritePin(RED_1_GPIO_Port, RED_1_Pin, GPIO_PIN_RESET);
 }
-void Phase1_RedOff()
-{
+void Phase1_RedOff(){
 	HAL_GPIO_WritePin(RED_1_GPIO_Port, RED_1_Pin, GPIO_PIN_SET);
 }
 
-void Phase2_GreenOn()
-{
+void Phase2_GreenOn(){
 	HAL_GPIO_WritePin(GREEN_2_GPIO_Port, GREEN_2_Pin, GPIO_PIN_RESET);
 }
-void Phase2_GreenOff()
-{
+void Phase2_GreenOff(){
 	HAL_GPIO_WritePin(GREEN_2_GPIO_Port, GREEN_2_Pin, GPIO_PIN_SET);
 }
 
-void Phase2_YellowOn()
-{
+void Phase2_YellowOn(){
 	HAL_GPIO_WritePin(YELLOW_2_GPIO_Port, YELLOW_2_Pin, GPIO_PIN_RESET);
 }
-void Phase2_YellowOff()
-{
+void Phase2_YellowOff(){
 	HAL_GPIO_WritePin(YELLOW_2_GPIO_Port, YELLOW_2_Pin, GPIO_PIN_SET);
 }
 
-void Phase2_RedOn()
-{
+void Phase2_RedOn(){
 	HAL_GPIO_WritePin(RED_2_GPIO_Port, RED_2_Pin, GPIO_PIN_RESET);
 }
-void Phase2_RedOff()
-{
+void Phase2_RedOff(){
 	HAL_GPIO_WritePin(RED_2_GPIO_Port, RED_2_Pin, GPIO_PIN_SET);
 }
 
